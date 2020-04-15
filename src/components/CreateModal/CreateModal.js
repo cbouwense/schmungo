d2f2a282a6a0b55896946bd72c13170b1c09820b
Christian Bouwense <bouwensec@gmail.com>
2020-04-15 18:36:55 -0400

    got the create modal basically good

--- src/components/CreateModal/CreateModal.js
+++ src/components/CreateModal/CreateModal.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "./CreateModal.css";
 
+//import { Aux } from "../../hoc/Aux/Aux";
+
 export const CreateModal = props => (
   props.open ? (
-    <div className={styles.Backdrop} onClick={props.close}>  
+    <div className={styles.ModalContainer}>
+      <div className={styles.Backdrop} onClick={props.close}></div>
       <div className={styles.CreateModal}>
         <div className={styles.ExitContainer}>
           <button
@@ -11,6 +14,23 @@ export const CreateModal = props => (
             onClick={props.close}
           >
             X
+          </button>
+        </div>
+        <h1 className={styles.Heading}>Title</h1>
+        <input 
+          className={styles.TitleInput}
+          type="text"
+        />
+        <h1 className={styles.Heading}>Description</h1>
+        <input 
+          className={styles.DescriptionInput}
+          type="text"
+        />
+        <div className={styles.SaveButtonContainer}>
+          <button 
+            className={styles.SaveButton}
+          >
+            Save
           </button>
         </div>
       </div>
