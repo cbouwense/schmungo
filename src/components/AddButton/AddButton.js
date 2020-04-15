2bfac20428122e42c4052c31f1a142ad9485337a
Christian Bouwense <bouwensec@gmail.com>
2020-04-15 16:42:29 -0400

    got the add button somewhat looking nice

---
+++ src/components/AddButton/AddButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./AddButton.css";
+
+export const AddButton = () => (
+  <button className={styles.AddButton}>
+    <p className={styles.ButtonText}>+</p>
+  </button>
+);
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import styles from './App.css';
+import React, { Component } from "react";
+import styles from "./App.css";
 
-import { Header } from '../Header/Header';
+import { AddButton } from "../AddButton/AddButton";
+import { Header } from "../Header/Header";
 
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
         <Header />
-        <p className={styles.AppBody}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className={styles.AppBody}>
+          <AddButton />
+        </div>
       </div>
     );
   }
