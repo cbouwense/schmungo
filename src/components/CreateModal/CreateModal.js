5cad7c85a81de43da72593f7679e11a32c218d15
Christian Bouwense <bouwensec@gmail.com>
2020-04-15 18:57:18 -0400

    got the form values to be saved in state.

--- src/components/CreateModal/CreateModal.js
+++ src/components/CreateModal/CreateModal.js
@@ -1,39 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./CreateModal.css";
 
-//import { Aux } from "../../hoc/Aux/Aux";
+export class CreateModal extends Component {
 
-export const CreateModal = props => (
-  props.open ? (
-    <div className={styles.ModalContainer}>
-      <div className={styles.Backdrop} onClick={props.close}></div>
-      <div className={styles.CreateModal}>
-        <div className={styles.ExitContainer}>
-          <button
-            className={styles.Exit}
-            onClick={props.close}
-          >
-            X
-          </button>
-        </div>
-        <h1 className={styles.Heading}>Title</h1>
-        <input 
-          className={styles.TitleInput}
-          type="text"
-        />
-        <h1 className={styles.Heading}>Description</h1>
-        <input 
-          className={styles.DescriptionInput}
-          type="text"
-        />
-        <div className={styles.SaveButtonContainer}>
-          <button 
-            className={styles.SaveButton}
-          >
-            Save
-          </button>
+  state = {
+    title: "",
+    description: ""
+  }
+
+  handleTitleChange = (event) => {
+    this.setState({
+      title: event.target.value
+    });
+  }
+
+  handleDescriptionChange = (event) => {
+    this.setState({
+      description: event.target.value
+    });
+  }
+
+  render() {
+    return this.props.open ? (
+      <div className={styles.ModalContainer}>
+        <div className={styles.Backdrop} onClick={this.props.close}></div>
+        <div className={styles.CreateModal}>
+          <div className={styles.ExitContainer}>
+            <button
+              className={styles.Exit}
+              onClick={this.props.close}
+            >
+              X
+            </button>
+          </div>
+          <h1 className={styles.Heading}>Title</h1>
+          <input 
+            className={styles.TitleInput}
+            type="text"
+            onChange={this.handleTitleChange}
+            value={this.state.title}
+          />
+          <h1 className={styles.Heading}>Description</h1>
+          <textarea 
+            className={styles.DescriptionInput}
+            onChange={this.handleDescriptionChange}
+            value={this.state.description}
+          />
+          <div className={styles.SaveButtonContainer}>
+            <button 
+              className={styles.SaveButton}
+            >
+              Save
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-   ) : null
-);
+     ) : null
+  }
+}
